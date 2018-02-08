fc6b9bb5fa74368a64be2f7299d848c50a153eb5
rohan <rhn.vma@gmail.com>
2018-02-08 15:40:55 +0530

    added get/todos/id

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var {
+  mongoose
+} = require('./db/mongoose');
+var {
+  Todo
+} = require('./models/todo');
+var {
+  User
+} = require('./models/user');
 
-;
+const {
+  ObjectID
+} = require('mongodb');
+
 
 var app = express();
-
 
 app.use(bodyParser.json());
 
@@ -26,14 +34,45 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos});
+    res.send({
+      todos
+    });
   }, (e) => {
     res.status(400).send(e);
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+
+
+app.get('/todos/:id', (req, res) => {
+
+      var id = req.params.id;
+
+      console.log("URL param: ", id);
+
+      if (!ObjectID.isValid(id)) {
+        return res.status(404).send("Invalid ID");
+      }
+
+      Todo.findById(id).then((todo) => {
+        if (!todo) {
+          return res.status(404).send("Todo not found");
+        }
+
+        res.status(200).send({todo});
+
+
+      }).catch((e) => {
+        res.status(400).send("ID not found");
+      });
+
 });
 
-module.exports = {app};
+
+      app.listen(3000, () => {
+        console.log('Started on port 3000');
+      });
+
+      module.exports = {
+        app
+      }
