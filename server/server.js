4bd39da3f8f581f4197326ad05fcbbb3fda96bfb
rohan <rhn.vma@gmail.com>
2018-02-09 14:23:13 +0530

    setup for heroku

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const port = process.env.PORT || 3000;
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -46,33 +48,35 @@ app.get('/todos', (req, res) => {
 
 app.get('/todos/:id', (req, res) => {
 
-      var id = req.params.id;
+  var id = req.params.id;
 
-      console.log("URL param: ", id);
+  console.log("URL param: ", id);
 
-      if (!ObjectID.isValid(id)) {
-        return res.status(404).send("Invalid ID");
-      }
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send("Invalid ID");
+  }
 
-      Todo.findById(id).then((todo) => {
-        if (!todo) {
-          return res.status(404).send("Todo not found");
-        }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send("Todo not found");
+    }
 
-        res.status(200).send({todo});
+    res.status(200).send({
+      todo
+    });
 
 
-      }).catch((e) => {
-        res.status(400).send("ID not found");
-      });
+  }).catch((e) => {
+    res.status(400).send("ID not found");
+  });
 
 });
 
 
-      app.listen(3000, () => {
-        console.log('Started on port 3000');
-      });
+app.listen(port, () => {
+  console.log('Started on port ${port}');
+});
 
-      module.exports = {
-        app
-      }
+module.exports = {
+  app
+}
