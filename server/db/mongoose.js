9f1f7833c3bf70e98e3c4a3dc9fefd8a94828c47
rohan <rhn.vma@gmail.com>
2018-02-11 22:59:34 +0530

    Added test db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp').then(
-  ()=> {console.log('MongoDB connected');},
-  err => {console.log(err);}
-);
+mongoose.connect(process.env.MONGODB_URI);
 
 
 module.exports = {mongoose};
