fb5a483799fcfaf9cb9e294b7aff197d14fb116c
rohan <rhn.vma@gmail.com>
2018-02-14 01:18:33 +0530

    added password hash

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,19 +1,34 @@
 const{SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
 
-var data = {
+var password ='123abc!';
 
-  id: 10
-};
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password,salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var token = jwt.sign( data, '123abc');
+var hashedPassword = '$2a$10$1kfrf84Jj7L9Dc3cr2ApKeXWSTjsjVtolYgrLrYyiqKnRAZ6TqzKi';
 
+bcrypt.compare(password, hashedPassword,(err, res) => {
+  console.log(res);
+})
 
-console.log(token);
-
-
- var decoded = jwt.verify(token, '123abc');
- console.log(decoded);
+// var data = {
+//
+//   id: 10
+// };
+//
+// var token = jwt.sign( data, '123abc');
+//
+//
+// console.log(token);
+//
+//
+//  var decoded = jwt.verify(token, '123abc');
+//  console.log(decoded);
 
 
 // var message = 'I am user number 7';
