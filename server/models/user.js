e3089648d8e76cb7aca33db3c670296c51fe05a5
rohan <rhn.vma@gmail.com>
2018-02-23 14:55:24 +0530

    user delete tokem

--- server/models/user.js
+++ server/models/user.js
@@ -53,6 +53,18 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+
+UserSchema.methods.removeToken = function(token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+      }
+    });
+  }
+
+
 UserSchema.pre('save', function (next) {
   var user = this;
 
