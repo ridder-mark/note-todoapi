c81361fe0e66e26a02702f1bc44b7243635e4079
rohan <rhn.vma@gmail.com>
2018-02-23 15:34:38 +0530

    associate users with todos

--- server/server.js
+++ server/server.js
@@ -19,9 +19,10 @@ app.use(bodyParser.json());
 
 // API routes
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    _creator: req.user._id
   });
 
   todo.save().then((doc) => {
@@ -32,8 +33,10 @@ app.post('/todos', (req, res) => {
 });
 
 
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+  Todo.find({
+    _creator: req.user._id
+  }).then((todos) => {
     res.status(200).send({
       todos
     });
@@ -44,17 +47,20 @@ app.get('/todos', (req, res) => {
 
 
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
 
   var id = req.params.id;
-
-  console.log("URL param: ", id);
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send("Invalid ID");
   }
 
-  Todo.findById(id).then((todo) => {
+  Todo.findOne({
+
+    _id: id,
+    _creator: req.user._id
+
+  }).then((todo) => {
     if (!todo) {
       return res.status(404).send("Todo not found");
     }
@@ -70,7 +76,7 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate, (req, res) => {
 
   var id = req.params.id;
 
@@ -78,7 +84,12 @@ app.delete('/todos/:id', (req, res) => {
     return res.status(404).send("Invalid ID");
   }
 
-  Todo.findByIdAndRemove(id).then((todo) => {
+  Todo.findOneAndRemove({
+
+    _id: id,
+    _creator: req.user._id
+
+  }).then((todo) => {
     if (!todo) {
       return res.status(404).send('Todo ID not found!');
     }
@@ -93,7 +104,7 @@ app.delete('/todos/:id', (req, res) => {
 
 
 
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
 
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
@@ -110,7 +121,12 @@ app.patch('/todos/:id', (req, res) => {
   }
 
 
-  Todo.findByIdAndUpdate(id, {
+  Todo.findOneAndUpdate({
+
+    _id: id,
+    _creator: req.user._id
+
+  }, {
     $set: body
   }, {
     new: true
@@ -181,23 +197,6 @@ app.delete('/users/me/token', authenticate, (req, res) => {
     res.status(400).send();
   });
 });
-
-
-// UserSchema.pre('save', function (next) {
-//   var user = this;
-//
-//   if(user.isModified('password')) {
-//
-//     bcrypt.genSalt(10, (err, salt) => {
-//       bcrypt.hash(user.password,salt, (err, hash) => {
-//         user.password = hash;
-//         next();
-//
-//       });
-//     });
-//
-//   } else { next();}
-// });
 
 
 
